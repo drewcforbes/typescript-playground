c259e789595aedc88b411dcb39a14a4fe28f6b20
Drew Forbes <andrew_c_forbes@homedepot.com>
2019-01-04 15:50:18 -0600

    Finish default parameter example.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-const world = '🗺️';
+/**
+ * Default parameter ##########################################################
+ */
 
-export function hello(word: string = world): string {
-    return `Hello ${world}! `;
+const worldEmoji = '🗺️';
+
+// export function hello(word: string = world): string {
+export function hello(word: string = worldEmoji): string {
+    return `Hello ${word} ! `;
 }
+
+console.log(hello('not a default parameter'));
+console.log();
+console.log(hello());
+console.log();
+console.log(hello);
+
+// ############################################################################
