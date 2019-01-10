7a11688e2e9a27f22ad5f4ca4bf92fccfd61dfa1
Drew Forbes <andrew_c_forbes@homedepot.com>
2019-01-10 17:29:18 -0600

    Checking in a few minutes of work

--- src/aoc-01/aoc01.ts
+++ src/aoc-01/aoc01.ts
@@ -9,12 +9,16 @@ import * as fs from 'fs';
 const data: string = fs.readFileSync('src/aoc-01/input.txt', 'utf8');
 const dataByLine: string[] = data.split('\n');
 
-let sum: bigint = 0n;
+// let sum: bigint = 0n;
+let sum = 0;
 
 for (const entry of dataByLine) {
 
+    // const num: number = parseInt(entry, 10);
+    // sum += bigint(num);
     const num: number = parseInt(entry, 10);
-    sum += bigint(num);
+    sum += num;
+    console.log('num ' + num + "   sum " + sum);
 
     // const firstChar: string = entry.slice(0, 1);
     // console.log(firstChar);
